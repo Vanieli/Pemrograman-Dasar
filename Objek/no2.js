1365d055b8e4fa9e1d230c1b367bc47fdf820f78
Vaniel <bryanivan4578@gmail.com>
2023-04-12 20:31:17 +0700

    Tugas Praktek 12/04/23

---
+++ Objek/no2.js
@@ -0,0 +1,8 @@
+let arrayDaftarPeserta = ["John Doe", "laki-laki", "baca buku" , 1992];
+
+let objDaftarPeserta ={
+    nama: arrayDaftarPeserta[0],
+    enisKelamin: arrayDaftarPeserta[1],
+    hobi: arrayDaftarPeserta[2],
+    tglLahir: arrayDaftarPeserta[3]
+}
